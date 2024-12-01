4394498fc2c2a5a59fa7e1fe5ed2c427c13c7fc7
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2024-12-01 23:59:25 +0100

    highlight code

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,59 +1,34 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextRequest } from "next/server";
 
-// export async function GET() {
-//     const GEMINI_API_KEY = process.env.GEMINI_API_KEY
-
-//     if (!GEMINI_API_KEY) return Response.json({ message: 'No API KEY...' })
-
-//     const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
-//     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-//     const prompt = "What is Next.js?";
-
-//     const result = await model.generateContent(prompt);
-//     const message = result.response.text()
-
-
-//     return Response.json({ message })
-// }
-
-
 export async function POST(request: NextRequest) {
-  const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+	const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
-  if (!GEMINI_API_KEY) {
-    return Response.json({ message: 'No API KEY...' }, { status: 500 });
-  }
+	if (!GEMINI_API_KEY) {
+		return Response.json({ message: "No API KEY..." }, { status: 500 });
+	}
 
-  const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+	const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+	const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-  try {
-    const { prompt, history } = await request.json();
+	try {
+		const { prompt, history } = await request.json();
 
-    //===========================================
+		//===========================================
 
-    const chat = model.startChat({
-      history
-      // history: [
-      //   {
-      //     role: "user",
-      //     parts: [{ text: "Hello" }],
-      //   },
-      //   {
-      //     role: "model",
-      //     parts: [{ text: "Great to meet you. What would you like to know?" }],
-      //   },
-      // ],
-    });
-    const result = await chat.sendMessage(prompt);
-    //============================================
-    const message = result.response.text();
+		const chat = model.startChat({
+			history,
+		});
+		const result = await chat.sendMessage(prompt);
+		//============================================
+		const message = result.response.text();
 
-    return Response.json({ message });
-  } catch (error: any) {
-    console.error("Error generating content:", error);
-    return Response.json({ message: 'Error generating content.', error: error.message }, { status: 500 });
-  }
+		return Response.json({ message });
+	} catch (error: unknown) {
+		console.error("Error generating content:", error);
+		return Response.json(
+			{ message: "Error generating content.", error },
+			{ status: 500 }
+		);
+	}
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 
 export const metadata: Metadata = {
+	authors: [{ name: "Vadim Gierko", url: "https://vadimgierko.com" }],
 	title: "Gemini AI Chat Next.js",
 	description: "Gemini AI chat written in TypeScript & Next.js",
 };
@@ -14,6 +15,15 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" data-bs-theme="dark">
+			<head>
+				{
+					// css for highlighting code in vs2015 dark mode style (without this rehype-highlight will not be highlighting)
+				}
+				<link
+					rel="stylesheet"
+					href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/vs2015.css"
+				/>
+			</head>
 			<body>{children}</body>
 		</html>
 	);
